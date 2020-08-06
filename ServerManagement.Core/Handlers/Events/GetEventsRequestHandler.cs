35f922266b83242d2c1c1eda6aedc48266cbfd90
alberto_guerrero <albrt83@gmail.com>
2020-08-06 13:00:43 -0400

    Add Windows Event Viewer support

---
+++ ServerManagement.Core/Handlers/Events/GetEventsRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using ServerManagement.Core.Requests.Events;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerManagement.Core.Handlers.Events
+{
+    public class GetEventsRequestHandler : IRequestHandler<GetEventsRequest, List<EventLog>>
+    {
+        public Task<List<EventLog>> Handle(GetEventsRequest request, CancellationToken cancellationToken)
+        {
+            var events = EventLog.GetEventLogs(request.ComputerName)
+                .ToList();
+
+            return Task.FromResult(events);
+        }
+    }
+}
---
+++ ServerManagement.Core/Requests/Events/GetEventsRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ServerManagement.Core.Requests.Events
+{
+    public class GetEventsRequest : IRequest<List<EventLog>>
+    {
+        public string ComputerName { get; set; } = Environment.MachineName;
+    }
+}
